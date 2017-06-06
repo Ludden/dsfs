f66b4f38a091bafbdcf4c0157cb6a88b908ec9f7
Johan Lundahl <johan.lundahl@gmail.com>
2017-06-06 18:10:39 +0200

    Add twitter client

---
+++ ch9/TwitterClient.py
@@ -0,0 +1,36 @@
+import json
+from collections import Counter
+
+from twython import TwythonStreamer
+
+key = json.loads(open("credentials.json").read())["key"]
+secret = json.loads(open("credentials.json").read())["secret"]
+token = json.loads(open("credentials.json").read())["token"]
+token_secret = json.loads(open("credentials.json").read())["token-secret"]
+
+tweets = []
+
+
+class TwitterStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if data['lang'] == 'en':
+            tweets.append(data)
+            print "received tweet #", len(tweets)
+
+        if len(tweets) > 100:
+            self.disconnect()
+
+    def on_error(self, status_code, data):
+        print status_code, data
+        self.disconnect()
+
+
+stream = TwitterStreamer(key, secret, token, token_secret)
+stream.statuses.filter(track='data')
+stream.statuses.sample()
+
+top_hashtags = Counter(hashtag['text'].lower()
+                       for tweet in tweets
+                       for hashtag in tweet["entities"]["hashtags"])
+
+print top_hashtags.most_common(5)
